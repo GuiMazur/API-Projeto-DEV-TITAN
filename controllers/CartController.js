84eccae585ae8b6183326841822c3278809c230a
Guilherme Mazur <jjguilh@gmail.com>
2022-09-10 00:18:11 -0300

    feat: adding and improving cart functions

---
+++ controllers/CartController.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+
+// { id: 1, quantity: 5}
+
+module.exports = {
+    Store: (req, res) => {
+        try{
+            const product = req.body
+            const userList = JSON.parse(fs.readFileSync('databases/users.json'))
+            const userIdx = userList.findIndex(o => o.id == req.params.id)
+            const user = userList[userIdx]
+            const produtoJaAdicionado = user.cart.find(o => o.id == product.id)
+
+            if(product.quantity <= 0) throw 'impossível comprar esta quantidade de produto'
+
+            if(produtoJaAdicionado){
+                const productIdx = user.cart.indexOf(produtoJaAdicionado)
+                user.cart[productIdx] = {
+                    ...produtoJaAdicionado, 
+                    quantity: produtoJaAdicionado.quantity+product.quantity
+                }                
+            } else{user.cart.push(product)}
+
+            userList[userIdx] = user            
+            fs.writeFileSync('databases/users.json', JSON.stringify(userList))
+            res.status(200).send('Produto adicionado ao carrinho')
+        } catch(e) {res.status(500).send(`Erro: ${e}`)}
+    },
+
+    Index: (req, res) => {
+        try{
+            const productList = JSON.parse(fs.readFileSync('databases/products.json'))
+            const userList = JSON.parse(fs.readFileSync('databases/users.json'))
+            const user = userList.find(o => o.id == req.params.id)
+
+            cart = user.cart.map((productInCart) => {
+                product = productList.find(product => product.id == productInCart.id)
+                return {...product, quantity: productInCart.quantity}
+            })
+            res.status(200).send(cart)
+        } catch(e){res.status(500).send(`Erro: ${e}`)}
+    },
+
+    Delete: (req, res) => {
+        try{
+            const userList = JSON.parse(fs.readFileSync('databases/users.json'))
+            const userIdx = userList.findIndex(o => o.id == req.params.user_id)
+            const user = userList[userIdx]
+            const productIdx = user.cart.findIndex(o => o.id == req.params.product_id)
+            if (productIdx > -1) {
+                user.cart.splice(productIdx, 1)
+                userList[userIdx] = user            
+                fs.writeFileSync('databases/users.json', JSON.stringify(userList))
+                res.status(200).send(user.cart)
+            } else throw 'Produto não encontrado'
+        } catch(e) {res.status(500).send(`Erro: ${e}`)}
+    }
+}
--- server.js
+++ server.js
@@ -2,14 +2,9 @@ const express = require('express');
 const app = express();
 const UserController = require('./controllers/UserController')
 const ProductController = require('./controllers/ProductController')
+const CartController = require('./controllers/CartController')
 
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -37,6 +32,14 @@ app.use((req, res, next) => {
   app.route('/products').get(ProductController.Index)
 
   app.route('/products/:id').get(ProductController.Show)
+
+    // CART
+
+  app.route('/cart/register/:id').post(CartController.Store)
+  
+  app.route('/cart/delete/:user_id/:product_id').delete(CartController.Delete)
+
+  app.route('/cart/:id').get(CartController.Index)
 
 // END ROUTES
 
